b2730cbb7c7393d92e97625d68878690a6d42cf4
carlosriosacevedo17 <48395676+carlosriosacevedo17@users.noreply.github.com>
2020-01-30 10:15:45 -0600

    Add files via upload

---
+++ hospitaldbbkp.sql
@@ -0,0 +1,82 @@
+-- MySQL dump 10.16  Distrib 10.1.36-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: hospitaldb
+-- ------------------------------------------------------
+-- Server version	10.1.36-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `hospital`
+--
+drop database if exists hospitaldb;
+create database hospitaldb;
+use hospitaldb;
+
+DROP TABLE IF EXISTS `hospital`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hospital` (
+  `codigo_h` int(11) NOT NULL,
+  `nombre` varchar(100) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`codigo_h`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hospital`
+--
+
+LOCK TABLES `hospital` WRITE;
+/*!40000 ALTER TABLE `hospital` DISABLE KEYS */;
+INSERT INTO `hospital` VALUES (100,'ANGELES'),(200,'PUEBLA'),(300,'BETANIA'),(400,'HUP'),(500,'GUADALUPE');
+/*!40000 ALTER TABLE `hospital` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sala`
+--
+
+DROP TABLE IF EXISTS `sala`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sala` (
+  `codigo_s` int(11) NOT NULL,
+  `nombre` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `num_camas` int(11) NOT NULL,
+  `codigo_h` int(11) NOT NULL,
+  PRIMARY KEY (`codigo_s`),
+  KEY `codigo_h` (`codigo_h`),
+  CONSTRAINT `sala_ibfk_1` FOREIGN KEY (`codigo_h`) REFERENCES `hospital` (`codigo_h`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sala`
+--
+
+LOCK TABLES `sala` WRITE;
+/*!40000 ALTER TABLE `sala` DISABLE KEYS */;
+INSERT INTO `sala` VALUES (1,'URGENCIAS',20,100),(2,'PEDIATRIA',5,100),(3,'TRAUMATOLOGIA',10,200),(4,'ESPERA',2,200),(5,'ORTOPEDIA',20,300),(6,'RAYOS X',1,400),(7,'TERAPIA INTENSIVA',10,400),(8,'ONCOLOGIA',20,400),(9,'NUTRICION',2,500);
+/*!40000 ALTER TABLE `sala` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-01-30 10:03:48
